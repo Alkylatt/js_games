753a642a639fa14e1afa8490e485029a71dd5d41
Alkylatt <sean05242012@yahoo.com>
2023-10-10 13:27:00 +0800

    fixed mine spawning, new problem: too much recursion

--- 8_minesweeper/script.js
+++ 8_minesweeper/script.js
@@ -1,6 +1,7 @@
 class GameSpace {
     grid_x; grid_y;
     mines = []; // location of all mines
+    untouched_tiles = []; // tiles that are not yet clicked (booleans)
     GAME_GRID_CONTAINER = document.querySelector('#game_container');
     GAME_GRIDS;
     isGameStarted = false;
@@ -35,40 +36,46 @@ class GameSpace {
 
             this.GAME_GRIDS[x] = (arrayTemp);
         }
+
+        // generate numbers for untouched_tiles
+        for(let i=0;i < this.grid_x*this.grid_y;i++) {
+            this.untouched_tiles.push(true);
+        }
     }
 
     // NOTICE: spawning too many mine may freeze the system
     // maybe I'll fix this in the future
-    spawnMine(startingPoint, amount=10) {
-        let max = this.grid_x + this.grid_y - 2;
+    spawnMine(startingTile, amount=10) {
+        let max = this.grid_x*this.grid_y-1;
         let mine_set = 0; // counter for mines that have been set by this function
 
-        while(mine_set < amount) {
-            let random_number = Math.floor(max * Math.random());
-
-            // check if a mine is spawned at the starting tile
-            if(random_number == startingPoint) {
-                continue;
-            }
-            // check if a mine is already there
-            if(this.mines.includes(random_number)) {
-                continue;
-            }
-
-            this.mines.push(random_number);
-            mine_set++;
+        // create a bag of mine candidate tiles
+        let bag = [];
+        for(let i=0;i < max;i++) {
+            if(i==startingTile) continue;
+            bag.push(i);
         }
 
+        while(mine_set < amount) {
+            let random_number = Math.floor(bag.length * Math.random());
+            let tileFromBag = bag.splice(random_number, 1); // pop 1 item
+            this.mines.push(tileFromBag[0]); // from bag into this.mines
+            mine_set++;
+        }
 
         this.mines.sort(function(a, b) { return a - b; }); // sort array "mines"
         console.log("mine spawned: ", this.mines);
     }
 
     clickTile(x, y) {
+        console.log(x+this.grid_x*y);
+        if(!this.untouched_tiles[x+this.grid_x*y]) return; // if this tile is already clicked, stop function
+        this.untouched_tiles[x+this.grid_x*y] = true; // flag as touched
+
         console.log(x + ", " + y + " is clicked");
         if(!this.isGameStarted) {
             this.isGameStarted = true;
-            this.spawnMine(x + y*this.grid_y); // spawn mines on first click
+            this.spawnMine(x + y*this.grid_y, 30); // spawn mines on first click
         }
         this.GAME_GRIDS[x+1][y+1].className = "tile tile-revealed";
         
@@ -77,7 +84,6 @@ class GameSpace {
             this.GAME_GRIDS[x+1][y+1].className = "tile tile-revealed tile-mine";
             return;
         }
-
 
         // check for surrounding mines
         let check = [
@@ -91,7 +97,7 @@ class GameSpace {
             [x+1, y-1],
         ];
         let temp = [];
-        // remove negative numbers
+        // remove out-of-bounds tiles from check
         for(let i=0;i < check.length;i++) {
             if(check[i][0] < 0 || check[i][1] < 0 || check[i][0] >= this.grid_x || check[i][1] >= this.grid_x ) continue;
             temp.push(check[i]);
@@ -99,102 +105,41 @@ class GameSpace {
         check = temp;
 
         let mine_count = 0;
-        console.log(this.mines);
         for(let i=0;i < check.length;i++) {
-            console.log(check[i][0] + check[i][1]*this.grid_x);
             if(this.mines.includes(check[i][0] + check[i][1]*this.grid_x)) {
                 mine_count++;
             }
         }
 
-        // for(let i=0;i < check.length;i++) {
-        //     this.GAME_GRIDS[check[i][0]+1][check[i][1]+1].className = "tile tile-revealed tile-8";
-        // }
-
-
         this.GAME_GRIDS[x+1][y+1].className = "tile tile-revealed tile-" + mine_count;
         if(mine_count != 0) this.GAME_GRIDS[x+1][y+1].innerHTML = mine_count;
-    }
-}
-
-
-
-// this class is not used
-class SnakeBody {
-    static speed = 15; // frame per grid (the lower the faster)
-    static grow = 3; // length in grids (starts with 3)
-    static environment;
-    x;
-    y;
-    constructor(environment, pos_x, pos_y) {
-        this.life = SnakeBody.grow;
-        this.x = pos_x;
-        this.y = pos_y;
-        SnakeBody.environment = environment;
-    }
-    move() {
-        SnakeBody.environment.GAME_GRIDS[this.x][this.y].classList = 'occupied snake';
-        this.life--;
-        if(this.life <= 0) {
-            SnakeBody.environment.GAME_GRIDS[this.x][this.y].classList = '';
-            delete this;
+        else {
+            for(let i=0;i < check.length;i++) {
+                if(!this.untouched_tiles[check[i][0]+this.grid_x*check[i][1]]) continue;
+                clickTile(check[i][0], check[i][1]);
+            }
         }
     }
 }
-class SnakeHead extends SnakeBody {
-    orientation = 0; // 0 up, 1 right, 2 down, 3 left
-    x;
-    y;
-    bodies = [];
-    environment;
-    constructor(environment, starting_x, starting_y) {
-        super();
-        this.x = starting_x;
-        this.y = starting_y;
-        this.environment = environment;
-    }
-    turn(move_ori) {
-        if( Math.abs(move_ori - this.orientation) === 2 ) return; // stop if move is invalid
-        this.orientation = move_ori;
-        turnCd = true;
-    }
-    move() {
-        this.bodies.push(new SnakeBody(this.environment, this.x, this.y));
 
-        let next_x = this.x,
-            next_y = this.y;
 
-        // move x or y
-        if(this.orientation % 2) next_x -= this.orientation - 2;
-        if(!(this.orientation % 2)) next_y -= this.orientation - 1;
-        // cycle back if out of bound
-        if(next_x < 1) next_x = this.environment.grid_x;
-        else if(next_x > this.environment.grid_x) next_x = 1;
-        if(next_y < 1) next_y = this.environment.grid_y;
-        else if(next_y > this.environment.grid_y) next_y = 1;
-
-        this.checkCollision(next_x, next_y);
-        this.x = next_x;
-        this.y = next_y;
-        this.environment.GAME_GRIDS[this.x][this.y].classList = 'occupied snake';
-
-        for(let i=0;i < this.bodies.length;i++) {
-            this.bodies[i].move();
-        }
-        turnCd = false;
+// code I stole from the internet for shuffling array
+function shuffle(array) {
+    let currentIndex = array.length,  randomIndex;
+  
+    // While there remain elements to shuffle.
+    while (currentIndex > 0) {
+  
+      // Pick a remaining element.
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+  
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
     }
-    checkCollision(x, y) {
-        if(this.environment.GAME_GRIDS[x][y].classList.contains('occupied')) {
-            // game over
-            this.environment.GAME_GRIDS[this.x][this.y].classList = 'hit'; // TODO ??? why the fuck is this not working
-            gameOver();
-        }else if(this.environment.GAME_GRIDS[x][y].classList.contains('food')) {
-            // eat, grow longer
-            SnakeBody.grow++;
-            this.environment.spawnFood();
-            if(SnakeBody.speed > 1) SnakeBody.speed--;
-        }
-    }
+  
+    return array;
 }
 
 
@@ -203,46 +148,6 @@ function clickTile(x, y) {
     currentGame.clickTile(x, y);
 }
 
+
 const GAME_SPACE = new GameSpace(20, 10);
 let currentGame = GAME_SPACE;
-
-
-
-// let lastUpdate = Date.now();
-// let intervalTick = setInterval(tick, 16.6); // about 60 fps, this should be terminated when game ends
-// let timer = 0; // game timer
-// let timer_move = 0; // timer (frames) until snake move one grid
-// let turnCd = false;
-
-function tick() {
-    let now = Date.now(); // [ms]
-    let dt = now - lastUpdate;
-    lastUpdate = now;
-    timer += dt;
-
-    if(!turnCd) {
-        if(keyMap["ArrowUp"]) {
-            SNAKE.turn(0);
-        }else if(keyMap["ArrowRight"]) {
-            SNAKE.turn(1);
-        }else if(keyMap["ArrowDown"]) {
-            SNAKE.turn(2);
-        }else if(keyMap["ArrowLeft"]) {
-            SNAKE.turn(3);
-        }
-    }
-
-    timer_move--;
-}
-
-function gameOver() {
-    clearInterval(intervalTick);
-    console.log("Game Over");
-}
-
-// Key press detector
-let keyMap = {}; // a array for keys' stats
-onkeydown = onkeyup = function(e){
-    e = e || event; // to deal with IE
-    keyMap[e.key] = e.type === "keydown";
-}
